2126e4dc7655b3b860ce9813532c2357e9eb691a
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2023-01-12 19:00:20 -0300

    Crud

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,15 +1,27 @@
-import { addProduct } from './products/product.service';
+import { addProduct, products } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-  id: '1',
-  title: 'p1',
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  stock: 90,
-  category: {
-    id: '12',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    name: 'category1',
-  },
-});
+for (let i = 0; i < 50; i++) {
+  addProduct({
+    id: faker.datatype.uuid(),
+    title: faker.commerce.productName(),
+    description: faker.commerce.productDescription(),
+    image: faker.image.imageUrl(),
+    size: faker.helpers.arrayElement(['M', 'S', 'XL', 'L']),
+    color: faker.color.rgb(),
+    createdAt: faker.date.recent(),
+    updatedAt: faker.date.recent(),
+    stock: faker.datatype.number({ min: 10, max: 100 }),
+    category: {
+      id: faker.datatype.uuid(),
+      name: faker.commerce.department(),
+      createdAt: faker.date.recent(),
+      updatedAt: faker.date.recent(),
+    },
+    price: parseInt(faker.commerce.price(), 10),
+    isNew: faker.datatype.boolean(),
+    tags: faker.helpers.arrayElements(),
+  });
+}
+
+console.log(products);
--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -1,11 +1,17 @@
-import { BaseModel } from "../base.mode";
-import { Category } from "../categories/category.model";
+import { BaseModel } from '../base.mode';
+import { Category } from '../categories/category.model';
 
 export type Sizes = 'S' | 'M' | 'L' | 'XL';
 
 export interface Product extends BaseModel {
   title: string;
+  image: string;
+  description: string;
   stock: number;
+  color: string;
+  price: number;
   size?: Sizes;
   category: Category;
+  isNew: boolean;
+  tags: string[];
 }
