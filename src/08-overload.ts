7fa9f9a38bc113691f098441519548a6bccc5d29
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2023-01-12 16:55:13 -0300

    overload

---
+++ src/08-overload.ts
@@ -0,0 +1,16 @@
+// Nico => [N, i, c, o] => string => string[]
+// [N, i, c, o] => Nico => string[] => string
+
+function parseStr(input: string | string[]): string | string[] {
+  if(Array.isArray(input)) {
+    return input.join("");
+  } else {
+    return input.split("");
+  }
+}
+
+const rtaArray = parseStr("Nico");
+const rtaString = parseStr(["N", "i", "c", "o"]);
+
+console.log("rtaArray", rtaArray);
+console.log("rtaString", rtaString);
